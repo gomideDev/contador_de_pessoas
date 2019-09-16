460c1a715fc3b91cfb1456f51a05150763ddc1ec
Silas Sampaio <silas.dev@hotmail.com>
2019-09-16 18:34:03 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "contador de pessoas",
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget{
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>{
+
+  int _people = 0;
+  String _infoText = "Pode Entrar";
+
+  void _changePeople(int delta){
+    setState(() {
+      _people += delta;
+      if(_people <= 8 && _people >= 0){
+        _infoText = "Pode Entrar";
+      }else if(_people < 0){
+        _infoText = "Mundo invertido?!";
+      }else if(_people > 8){
+        _infoText = "Lotado";
+      }
+    });
+  }
+
+  @override 
+  Widget build(BuildContext context){
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+          Column( //Colum permite criar widgets em cima de outro na vertical
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          Text("Pessoas: $_people",
+            style: TextStyle(
+            color: Colors.white, 
+            fontWeight: FontWeight.bold)
+          ),
+          Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: 
+          <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                child: Text("+1",
+                style: TextStyle(
+                fontSize: 40.0, 
+                color: Colors.white),
+                ),
+                onPressed: (){
+                  _changePeople(1);
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                child: Text("-1",
+                style: TextStyle(
+                fontSize: 40.0, 
+                color: Colors.white),
+                ),
+                onPressed: (){
+                 _changePeople(-1);
+                },
+              ),
+            ),
+            ],
+          ),
+          Text(_infoText,
+          style: TextStyle(
+            color: Colors.white, 
+            fontStyle: FontStyle.italic,
+            fontSize: 30.0)
+          )
+        ],)
+      ],
+    );
+  }
+}
